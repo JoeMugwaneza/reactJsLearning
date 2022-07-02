d3ffa2c2ed2450e42e38d4f020d93fabf46bd933
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-07-02 23:47:05 +0200

    organized my file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import Header from './Header.js';
 import MainContent from './MainContent.js';
 import Footer from './Footer.js';
 import './styles/index.css';
+import React from 'react';
+import { render } from 'react-dom';
+
+
 
 function App(){
  return (
